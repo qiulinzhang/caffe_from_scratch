278ddd3ac79f587fe755958eec05db5e7228d743
Qiulin Zhang <1242526239@qq.com>
2022-09-14 20:27:16 +0800

    blob.cpp

--- src/caffe/blob.cpp
+++ src/caffe/blob.cpp
@@ -18,6 +18,7 @@ namespace caffe {
 
     template <typename Dtype>
     void Blob<Dtype>::Reshape(const vector<int>& shape) {
+        // problem 没有对里面的数据进行reshape??
         CHECK_LE(shape.size(), kMaxBlobAxes);
         count_ = 1;
         shape_.resize(shape.size());
@@ -55,4 +56,64 @@ namespace caffe {
     void Blob<Dtype>::ReshapeLike(const Blob<Dtype>& other) {
         Reshape(other.shape());
     }
-}
+
+    template <typename Dtype>
+    Blob<Dtype>::Blob(const int num, const int channels, const int height, const int width)
+        // capacity_ must be initialized before calling Reshape 
+        // 因为capacity_在Reshape函数中会直接使用，因此需要提前初始化
+        : capacity_(0) {
+            Reshape(num, channels, height, width);
+        }
+    
+    template <typename Dtype>
+    Blobs<Dtype>::Blob(const vector<int>& shape)
+        // capacity_ must be initialized before calling Reshape 
+        // 因为capacity_在Reshape函数中会直接使用，因此需要提前初始化
+        : capacity_(0) {
+            Reshape(shape);
+        }
+
+    template <typename Dtype>
+    const int* Blob<Dtype>::gpu_shape() const {
+        CHECK(shape_data_);
+        // 判断 shape_data_ 是否在GPU上，如果不在需要利用cuda将其搬运到GPU上
+        return (const int*)shape_data_->gpu_data();
+    }
+
+    template <typename Dtype>
+    const Dtype* Blob<Dtype>::cpu_data() const {
+        CHECK(data_);
+        // 判断 data_ 是否在CPU上，如果不在需要利用cuda将其搬运到CPU上
+        return (const Dtype*) data_->cpu_data();
+    }
+
+    template <typename Dtype>
+    void Blob<Dtype>::set_cpu_data(Dtype* data) {
+        CHECK(data);
+        // Make sure CPU and GPU sizes remains equal
+        size_t size = count_ * sizeof(Dtype);
+        if (data_->size()!=size) {
+            data_.reset(new SyncedMemory(size));
+            diff_.reset(new SyncedMemory(size));
+        }
+        data_->set_cpu_data(data);
+    }
+
+    template <typename Dtype>
+    const Dtype* Blob<Dtype>::gpu_data() const {
+        CHECK(data_);
+        // 判断 shape_data_ 是否在GPU上，如果不在需要利用cuda将其搬运到GPU上
+        return (const Dtype*) data_->gpu_data();
+    }
+
+    template <typename Dtype>
+    void Blob<Dtype>::set_gpu_data(Dtype* data) {
+        CHECK(data);
+        // Make sure CPU and GPU sizes remains equal
+        size_t size = count_ * sizeof(Dtype); // problem，为什么要比较这两个size
+        if (data_.size()!=size) {
+            data_.reset(new SyncedMemory(size));
+            diff_.reset(new SyncedMemory(size));
+        }
+        data_->set_gpu_data(data);
+    }
